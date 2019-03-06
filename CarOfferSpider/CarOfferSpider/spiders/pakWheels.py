0c71b2089589e6059f8583863959943bb15e32e4
Mohammad Usman <mohammadusman666@gmail.com>
2019-03-06 21:38:32 +0500

    Update pakWheels.py
    Added Comments, Updated getPrice function, Inserted getLocation and getImages function

--- CarOfferSpider/CarOfferSpider/spiders/pakWheels.py
+++ CarOfferSpider/CarOfferSpider/spiders/pakWheels.py
@@ -1,39 +1,75 @@
 # -*- coding: utf-8 -*-
 import scrapy
+# for string to list conversion
+import ast
 
+# TODO: check keyword in Title
 
-class PakwheelsSpider(scrapy.Spider):
-    name = 'pakWheels'
-    allowed_domains = ['pakwheels.com']
-    start_urls = ['https://www.pakwheels.com/used-cars/search/-/mk_honda/md_city/vr_i-vtec-prosmatec/ct_lahore/']
+class PakwheelsSpiderSpider(scrapy.Spider):
+    name = 'pakwheels_spider'
+    allowed_domains = ['www.pakwheels.com']
+    start_urls = ['https://www.pakwheels.com/used-cars/search/-/?q=city+2016']
 
     def parse(self, response):
-        # //*[@id="main_ad_3155615"]/div/div[2]/div[1]/div/div
+        # scrape URLs and Titles
         data = response.xpath('//div[@class="well search-list clearfix ad-container page-"]/div[2]/div[1]/div/div/a')
-        # //*[@id="main_ad_3155615"]/div/div[2]/div[2]/div/ul[2]
+        # scrape prices
+        prices = response.xpath('//div[@class="well search-list clearfix ad-container page-"]/div[2]/div[1]/div/div/div/div')
+        # scrape Locations
+        locations = response.xpath('//div[@class="well search-list clearfix ad-container page-"]/div[2]/div[2]/div/ul[1]')
+        # scrape other data (model, mileage, fuel type, engine, transmission)
         data1 = response.xpath('//div[@class="well search-list clearfix ad-container page-"]/div[2]/div[2]/div/ul[2]')
+        # scarpe Image URLs
+        images = response.xpath('//div[@class="well search-list clearfix ad-container page-"]/div[1]/div[1]/div')
 
+        # list of dictionaries each representing a vehicle
         items = []
 
         for i in range(len(data)):
             item = {}
-            item["url"] = self.getUrl(data[i])
-            item["title"] = self.getTitle(data[i])
-            item["modelDate"] = self.getModel(data1[i])
-            item["mileage"] = self.getMileage(data1[i])
-            item["fuelType"] = self.getFuel(data1[i])
-            item["engine"] = self.getEngine(data1[i])
-            item["transmission"] = self.getTransmission(data1[i])
+            item["url"] = self.getUrl(data[i]) # get URL
+            item["title"] = self.getTitle(data[i]) # get title
+            item["price"] = self.getPrice(prices[i]) # get price
+            item["location"] = self.getLocation(locations[i]) # get location
+            item["modelDate"] = self.getModel(data1[i]) # get model year
+            item["mileage"] = self.getMileage(data1[i]) # get mileage
+            item["fuelType"] = self.getFuel(data1[i]) # get fuel type
+            item["engine"] = self.getEngine(data1[i]) # get engine
+            item["transmission"] = self.getTransmission(data1[i]) # get transmission
+            item["images"] = self.getImages(images[i]) # get image URLs
             items.append(item)
             # yield item
 
-        print(items)
+        # print(items)
+
+        # write data in a file to view the data scraped
+        with open('file.txt', 'w') as f:
+            for item in items:
+                f.write("%s\n" % item)
 
     def getUrl(self, item):
+        # concatenating domain name with the path of the ad
         return self.allowed_domains[0] + "/" + item.xpath('@href').extract()[0]
 
     def getTitle(self, item):
         return item.xpath('@title').extract()[0]
+
+    def getPrice(self, item):
+        # if there is Call in the string, then return Call for Price, else return Price
+        temp = item.xpath('text()').extract()[0]
+        
+        if ("Call" in temp):
+            return "Call for Price"
+        else:
+            item = item.xpath('meta[1]')
+            return item.xpath('@content').extract()[0]
+
+    def getLocation(self, item):
+        # strip whitespace and newline
+        location = item.xpath('li/text()').extract()[0]
+        location = location.replace(" ", "")
+        location = location.replace("\n", "")
+        return location
 
     def getModel(self, item):
         return item.xpath('li[1]/text()').extract()[0]
@@ -50,13 +86,22 @@ class PakwheelsSpider(scrapy.Spider):
     def getTransmission(self, item):
         return item.xpath('li[5]/text()').extract()[0]
 
-    # def parse(self, response):
-    #     print(response.xpath('//ul/li[@itemtype="https://schema.org/Vehicle"][@name=$li]/preceding-sibling::li[1]/meta[@itemprop="name"]/@content').get())
+    def getImages(self, item):
+        divClass = item.xpath('@class').extract()
 
-#
-# //meta[@itemprop="name"]/@content
-#
-# //div[@id="images"]/a/text()'
+        # final list of images
+        images = []
 
+        if (divClass):
+            return images
+        else:
+            item = item.xpath('ul')
+            lst = item.xpath('@data-galleryinfo').extract()[0]
+            # convert string representing a list to an actual list
+            lst = ast.literal_eval(lst)
 
-# //meta[2][@itemprop="name"]/@content'
+            for i in range(len(lst)):
+                # scrapping image urls
+                images.append(lst[i]["src"])
+
+            return images
